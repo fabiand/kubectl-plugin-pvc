66cf066207abc43e129ce3a919c9983021959e0c
Fabian Deutsch <fabiand@fedoraproject.org>
2018-03-12 15:48:05 +0100

    Fix remote install URL
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ PLUGIN_CMD=pvc
 
 [[ -f "$PLUGIN_CMD" ]] || {
   echo "Pulling $PLUGIN_CMD from GitHub"
-  curl -L -o $PLUGIN_CMD https://github.com/fabiand/kubectl-plugin-pvc/blob/master/pvc
+  curl -L -o $PLUGIN_CMD \
+    https://raw.githubusercontent.com/fabiand/kubectl-plugin-pvc/master/pvc
 }
 
 mkdir -p "$PLUGIN_PATH"
